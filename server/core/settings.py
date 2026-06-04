117e35805a03a43350e73b7892986d3aba92445d
Freelance06 <orionetobia@gmail.com>
2026-06-05 06:45:51 +0800

    test

--- server/core/settings.py
+++ server/core/settings.py
@@ -98,7 +98,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=10),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
 }
 
